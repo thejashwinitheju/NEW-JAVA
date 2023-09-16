b06f88bccd0c87d1eb7e536495461279f7d7b581
thejashwinitheju <thejashwinitheju912@gmail.com>
2023-09-16 13:12:46 +0530

    ifstatement

---
+++ controlflow/ifst.java
@@ -0,0 +1,13 @@
+package controlflow;
+
+public class ifst {
+    public static void main (String[] args){
+        int a=10;
+        int b=20;
+
+        if(a+b>20){
+            System.out.println("A+B is greater");
+        }
+    }
+    
+}
