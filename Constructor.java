cf479db6e491886377e611f7146b289643cbbaa6
thejashwinitheju <thejashwinitheju912@gmail.com>
2023-09-16 17:28:21 +0530

    inheritnce

---
+++ Constructor.java
@@ -0,0 +1,14 @@
+public class Constructor {
+    String name ;
+    int rollno ;
+
+    Constructor(){
+      name = "theju";
+      rollno = 01; 
+    }
+    public static void main(String args[]){
+Constructor s1 =new Constructor();
+System.out.println(s1.name);
+    }
+    
+}
---
+++ Inheritance.java
@@ -0,0 +1,18 @@
+public class Inheritance {
+    String name="theju";
+
+}
+    
+    class Child extends Inheritance{
+           
+              String location="Aranakatte"; 
+   
+    
+
+    public static void main(String args[]){
+Child a = new Child();
+        System.out.println(a.name);
+        System.out.println(a.location);
+
+    }
+}
