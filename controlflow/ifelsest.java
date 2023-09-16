7933990e4f93b761668fbdeb9c24afb233e3092c
thejashwinitheju <thejashwinitheju912@gmail.com>
2023-09-16 13:17:54 +0530

    ifelsest

---
+++ controlflow/ifelsest.java
@@ -0,0 +1,14 @@
+package controlflow;
+
+public class ifelsest{
+    public static void main(String args[]){
+        int a=10;
+        int b=20;
+
+        if(a+b>40){
+            System.out.println("a+b is greater");
+        }else{
+            System.out.println("a+b id not greater than 40");
+        }
+    }
+}
