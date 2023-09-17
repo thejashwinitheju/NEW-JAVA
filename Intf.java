ea44a9038b6d78e5d76704e3344940da64ac1eef
thejashwinitheju <thejashwinitheju912@gmail.com>
2023-09-17 13:52:11 +0530

    interface

---
+++ Intf.java
@@ -0,0 +1,23 @@
+interface A{
+    void Adisplay();
+}
+
+/*interface B{
+    void Bdisplay();
+}*/
+
+abstract class Abc implements A{
+public void Adisplay(){
+System.out.println("hi");
+}
+public static void main(String args[]){
+    System.out.print("hello");
+}
+}
+class Amain extends Abc{
+    public static void main(String args[]){
+Abc obj=new Amain ();
+obj.Adisplay();
+//obj.Bdisplay();
+    }
+}
