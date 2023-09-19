b6424c3da2959e17c94dbd96e5a70fa692637776
thejashwinitheju <thejashwinitheju912@gmail.com>
2023-09-19 10:09:22 +0530

    applet

---
+++ Applet.java
@@ -0,0 +1,9 @@
+//import java.applet.Applet;  
+import java.awt.Graphics;  
+public class  Applet{  
+  
+public void paint(Graphics g){  
+g.drawString("welcome",150,150);  
+}  
+  
+}
--- Lengtheg.java
+++ Lengtheg.java
@@ -1,7 +1,8 @@
-public class Lengtheg{  
-public static void main(String args[]){  
-String s1="javatpoint";  
-String s2="python";  
-System.out.println("string length is: "+s1.length());//10 is the length of javatpoint string  
-System.out.println("string length is: "+s2.length());//6 is the length of python string  
-}} 
+
+public class Lengtheg {
+    public static void main(String args[]){
+       String s1="JAva";
+        //int length =str.length();
+        System.out.println("string length is" + s1.toLowerCase());
+    }
+}
