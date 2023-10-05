3d41f5dc8477b18032030ff791cb2ecb83339e4b
thejashwinitheju <thejashwinitheju912@gmail.com>
2023-10-05 22:46:24 +0530

    multiplay

---
+++ programs/multi.java
@@ -0,0 +1,13 @@
+package programs;
+
+public class multi {
+    public static void main(String args[]){
+        for(int i=1;i<=10;i++){
+            if(i%3==0){
+                System.out.println(i);
+
+            }
+
+        }
+    }
+}
